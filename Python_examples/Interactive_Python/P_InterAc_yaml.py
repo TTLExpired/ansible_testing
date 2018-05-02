6ec8fa8286981cb715d90956288f091bcadf78e8
TTLExpired <bill.mosses@gmail.com>
2018-05-02 17:48:45 +1000

    Testing Ansible Parsing

--- Python_examples/Interactive_Python/P_InterAc_yaml.py
+++ Python_examples/Interactive_Python/P_InterAc_yaml.py
@@ -1,6 +1,5 @@
 import yaml
 
-
 # Functions
 def GetValue(AssignedValue):
     """Confirm if current value is correct
@@ -17,36 +16,40 @@ def GetValue(AssignedValue):
     return AssignedValue
 
 
-print(" +++ Welcome to IOS writer +++")
-print(" IF you make a mistake at any time, press ctrl+c")
-print("let's begin")
-print('*'*60)
+def main():
+    print(" +++ Welcome to IOS writer +++")
+    print(" IF you make a mistake at any time, press ctrl+c")
+    print("let's begin")
+    print('*'*60)
 
-# Hostname and SNMP Chassis ID
-hostname = input("Hostname? ")
+    # Hostname and SNMP Chassis ID
+    hostname = input("Hostname? ")
 
-snmpid = input("Use hostname for snmp chassis-ID [Y/N]? ")
-if snmpid.lower() in ['yes', 'y']:
-    snmpchassis = hostname
-else:
-    snmpchassis = input(" Snmp Chassis-id? ")
-print("chassis id is {}".format(snmpchassis))
+    snmpid = input("Use hostname for snmp chassis-ID [Y/N]? ")
+    if snmpid.lower() in ['yes', 'y']:
+        snmpchassis = hostname
+    else:
+        snmpchassis = input(" Snmp Chassis-id? ")
+    print("chassis id is {}".format(snmpchassis))
 
-timezone = input("Timezone? ")
-fqdn = input(" Domain Name? ")
-if hostname.lower() in fqdn:
-    fqdn = input("Please enter domain name without hostname: ")
-else:
-    pass
-if timezone == 'ADST +10':
-    recurr = 'ADST recurring 1 Sun Oct 2:00 1 Sun Apr 3:00'
+    timezone = input("Timezone? ")
+    fqdn = input(" Domain Name? ")
+    if hostname.lower() in fqdn:
+        fqdn = input("Please enter domain name without hostname: ")
+    else:
+        pass
+    if timezone == 'ADST +10':
+        recurr = 'ADST recurring 1 Sun Oct 2:00 1 Sun Apr 3:00'
 
-stream = open('config.yaml', 'w')
-yaml.dump({'system': {'name': hostname, 'location': snmpchassis,
-                      'tz': timezone, 'fqdn': fqdn, 'recurr': recurr }},
-            stream, default_flow_style=False)
+    stream = open('config.yaml', 'w')
+    yaml.dump({'system': {'name': hostname, 'location': snmpchassis,
+                          'tz': timezone, 'fqdn': fqdn, 'recurr': recurr }},
+                stream, default_flow_style=False)
 
+    """
+    print(yaml.dump({'system': {'name': hostname, 'location': snmpchassis}},
+                    default_flow_style=False))
 """
-print(yaml.dump({'system': {'name': hostname, 'location': snmpchassis}},
-                default_flow_style=False))
-"""
+if __name__ == "__main__":
+    main()
+
