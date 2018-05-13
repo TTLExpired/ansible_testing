027b7b9829fb80ee43e5844db780727e318d4558
TTLExpired <bill.mosses@gmail.com>
2018-05-13 20:04:15 +1000

    More junk files for testing

---
+++ python_examples/Python_examples/sample_1.py
@@ -0,0 +1,27 @@
+# A quick script to input n number in a list and display them.
+
+List = []
+ListLength = input("How many entries in your list? ")
+
+while True:
+    try:
+        Check_Value = int(ListLength)
+        if Check_Value:
+            print("You entered {}. Will proceed.".format(Check_Value))
+            break
+    except ValueError:
+        ListLength = input("How many entries in your list? ")
+
+print("We'll input {}".format(ListLength))
+for i in range(int(ListLength)):
+
+    while True:
+        ListValue = input("Enter Number: ")
+        try:
+            if int(ListValue):
+                List.append(ListValue)
+                break
+        except ValueError:
+            ListValue = input("Numbers only!: ")
+
+print("The list is {}".format(List))
