a24553d9b7103877aaab0563d7266844043355bb
TTLExpired <bill.mosses@gmail.com>
2018-06-05 17:24:44 +1000

    testing

--- python_examples/excel_python/P_excelsheet_to_yaml.py
+++ python_examples/excel_python/P_excelsheet_to_yaml.py
@@ -12,25 +12,21 @@ def convert_sheet_list(sheet, xrows, ycolumns):
     rows = []
     for first_row in range(0, xrows):
         values = []
-        value = (sheet.cell(first_row, 0).value)
-        if value != '':
+        key_value = (sheet.cell(first_row, 0).value)
+        if key_value != '':
             internal_value = []
-            values.insert(first_row, value)
-            row_position = first_row
-            for second_row in range(1, xrows):
-                second_value = (sheet.cell(second_row, 0).value)
-                if second_value == '':
-                    for col in range(1, ycolumns):
-                        if ycolumns != '':
-                            value = (sheet.cell(second_row, col).value)
-                            if value != '':
-                                try:
-                                    value = str(int(value))
-                                except ValueError:
-                                    pass
-                                finally:
-                                    internal_value.append(value)
-            values.insert(row_position, internal_value)
+            values.insert(first_row, key_value)
+        for col in range(1, ycolumns):
+            data_value = (sheet.cell(first_row, col).value)
+            if data_value != '':
+                try:
+                    data_value = str(int(data_value))
+                except ValueError:
+                    pass
+                finally:
+                    internal_value.append(data_value)
+
+                values.append(data_value)
 
         rows.append(values)
 
